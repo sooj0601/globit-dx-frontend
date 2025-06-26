877c828c597999ac870f8fd4f873e40dad1b09c6
정수정 <soo.j0601@gmail.com>
2025-06-26 14:52:14 +0900

    웹폰트 안나오는 문제 수정

--- app/root.tsx
+++ app/root.tsx
@@ -24,6 +24,10 @@ export const links: Route.LinksFunction = () => [
     rel: 'stylesheet',
     href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
   },
+  {
+    rel: 'stylesheet',
+    href: 'https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css',
+  },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
